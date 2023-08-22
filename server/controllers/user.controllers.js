7f70469d0202a94add8a98e2857d190481ded62c
RANJAN PALAI <palairanjan2002@gmail.com>
2023-08-22 18:03:01 +0530

    done with login functionality and jwt token

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -1,6 +1,7 @@
 const { User } = require("../models/user.model");
 const bcrypt=require("bcrypt");
-
+const jwt=require("jsonwebtoken");
+require("dotenv").config();
 
 // register user and hashing the password 
 const register=async(req,res)=>{
@@ -22,4 +23,26 @@ const register=async(req,res)=>{
     }
 }
 
-module.exports = {register}
+
+const login=async(req,res)=>{
+    const {email,password} = req.body;
+    try {
+    const existingUser=await User.findOne({email});
+    if(!existingUser){
+        return res.status(404).json({message:"User not found"});
+    }
+    const matchPassword=await bcrypt.compare(password, existingUser.password);
+    if(!matchPassword){
+        return res.status(400).json({message:"Invalid Credentials"});
+    }
+    const token=jwt.sign({email:existingUser.email,id:existingUser._id},process.env.SECRET_KEY);
+    res.status(200).json({token:token,user:existingUser}); 
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"login failed",error:error})
+    }
+}
+
+
+
+module.exports = {register,login}
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,14 +1,12 @@
 const express=require("express");
-const { register } = require("../controllers/user.controllers");
+const { register, login } = require("../controllers/user.controllers");
 
 const userRouter=express.Router();
 
 
 userRouter.post("/register",register)
 
-userRouter.post("/login",(req,res)=>{
-    res.send("login")
-})
+userRouter.post("/login",login)
 
 
 module.exports ={userRouter}
